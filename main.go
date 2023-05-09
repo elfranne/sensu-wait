d4f09bb79f9ab2f06b9a0b86a080bf53925e2119
Elfranne <tom.braarup@gmail.com>
2023-05-09 13:12:41 +0200

    first build

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"time"
 
-	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-go/types"
 	"github.com/sensu/sensu-plugin-sdk/sensu"
 )
 
 // Config represents the handler plugin config.
 type Config struct {
 	sensu.PluginConfig
-	Example string
+	Wait int
 }
+
+const (
+	wait = 1
+)
 
 var (
 	plugin = Config{
@@ -24,14 +28,14 @@ var (
 	}
 
 	options = []sensu.ConfigOption{
-		&sensu.PluginConfigOption[string]{
-			Path:      "example",
-			Env:       "HANDLER_EXAMPLE",
-			Argument:  "example",
-			Shorthand: "e",
-			Default:   "",
-			Usage:     "An example string configuration option",
-			Value:     &plugin.Example,
+		&sensu.PluginConfigOption[int]{
+			Path:      "wait",
+			Env:       "SENSU_WAIT",
+			Argument:  "wait",
+			Shorthand: "-w",
+			Default:   int(wait),
+			Usage:     "Time to wait in seconds",
+			Value:     &plugin.Wait,
 		},
 	}
 )
@@ -41,14 +45,13 @@ func main() {
 	handler.Execute()
 }
 
-func checkArgs(event *corev2.Event) error {
-	if len(plugin.Example) == 0 {
-		return fmt.Errorf("--example or HANDLER_EXAMPLE environment variable is required")
-	}
+func checkArgs(_ *types.Event) error {
+	// nothing really to check, default is 1 second, type is integer
 	return nil
 }
 
-func executeHandler(event *corev2.Event) error {
-	log.Println("executing handler with --example", plugin.Example)
+func executeHandler(event *types.Event) error {
+	time.Sleep(time.Duration(plugin.Wait) * time.Second)
+	log.Printf("Waiting for %v seconds", plugin.Wait)
 	return nil
 }
